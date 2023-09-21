6d02d8aa933ee822f858efc01117dca9d757b80f
Santhoshkumar <99131883+iamsansk@users.noreply.github.com>
2023-09-21 08:52:19 +0530

    Update script.js
    Resolved the reload problem when deleting the todo and optimize Javascript code

--- script.js
+++ script.js
@@ -1,70 +1,59 @@
-const addBtn = document.getElementById("addBtn");
-const input = document.getElementById("input");
-const invalidMsg = document.getElementById('invalid');
-const removeEl = document.getElementsByTagName("i");
+const addBtn = document.querySelector("#addBtn");
+const input = document.querySelector("#input");
+const invalidMsg = document.querySelector('#invalid');
+const list = document.querySelector("#list");
 
-let index=0;
 let listArr = [];
-window.onload = ()=>{
+
+window.onload = () => {
     load();
 }
-function load(){
-    listArr = JSON.parse(localStorage.getItem("todos"))||[];
-    listArr.forEach(todo=>{
+
+function load() {
+    listArr = JSON.parse(localStorage.getItem("todos")) || [];
+    listArr.forEach(todo => {
         displayTodo(todo);
     });
 }
-function addItems(){
-    let todo = input.value;
-    listArr.push(todo);
-    localStorage.setItem("todos",JSON.stringify(listArr))
-    displayTodo(todo);
-}
-function displayTodo(todo){
-    let listItems = document.createElement("li");
-    listItems.id=`list${index}`;
-    listItems.innerHTML = `
-    <input type="checkbox" id="task${index}">
-    <label for="task${index}">${todo}</label>
-    <i onclick=removeItem(${index})></i>`;
-    document.getElementById("list").appendChild(listItems);
-    input.value="";
-    index++;
-}
-function removeItem(index){
-    console.log(index);
-    if(index>-1){
-        listArr.splice(index,1);
-        localStorage.setItem("todos",JSON.stringify(listArr));
-        location.reload();
-    }
-    updateList(index);
-}
-function updateList(index){
-    let listId = `list${index}`;
-    let li = document.getElementById(listId);
-    document.getElementById("list").removeChild(li);
-}
 
-addBtn.addEventListener("click",()=>{
-    if(input.value==="" || input.value===" "){
+function addItems() {
+    const todo = input.value.trim(); 
+    if (todo !== "") {
+        listArr.push(todo);
+        localStorage.setItem("todos", JSON.stringify(listArr))
+        displayTodo(todo);
+        input.value = "";
+    } else {
         input.style.border = "2px solid red";
         invalidMsg.innerHTML = "*Empty value is invalid";
     }
-    else{
-        input.style.border = "none";
-        invalidMsg.innerHTML = "";
+}
+
+function displayTodo(todo) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+        <input type="checkbox" id='label'>
+        <label for='label'>${todo}</label>
+        <i class="remove-btn"></i>`;
+    list.appendChild(listItem);
+}
+
+list.addEventListener("click", (event) => {
+    if (event.target.classList.contains("remove-btn")) {
+        const listItem = event.target.parentElement;
+        console.log(listItem);
+        const index = Array.from(list.children).indexOf(listItem);
+        console.log(index);
+        listArr.splice(index, 1);
+        localStorage.setItem("todos", JSON.stringify(listArr));
+        list.removeChild(listItem);
+    }
+});
+
+addBtn.addEventListener("click", addItems);
+
+input.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
         addItems();
     }
 });
-input.addEventListener("keyup",function(event){
-    if(event.key=="Enter" && input.value!=="" && input.value!==" "){
-        input.style.border = "none";
-        invalidMsg.innerHTML = "";
-        addItems();
-    }
-    else if(event.key=="Enter" && input.value==="" || input.value===" "){
-        input.style.border = "2px solid red"; 
-        invalidMsg.innerHTML = "*Empty value is invalid"
-    }
-})
